493814d41a2551623c5a2e301c01d0f7ad40c05b
Eldad <nevoEldad@gmail.com>
2021-04-22 19:54:41 +0300

    socket io init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
-  res.send('HELLO');
+io.on('connection', () => {
+  console.log('New Websocket connection');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Hi');
 });
