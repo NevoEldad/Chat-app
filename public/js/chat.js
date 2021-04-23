a86475cb3b1fec2b24dc402a05cba85aec8351d4
NevoEldad <nevoeldad@gmail.com>
2021-04-23 14:28:33 +0300

    geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,41 @@
 const socket = io();
 
-socket.on('message', (data) => {
+socket.on('message', data => {
+  console.log('MESSAGE GOT IN');
+
   console.log(data);
 });
 
 const sendButton = document.querySelector('#sendMessage');
 const messageContent = document.querySelector('#message');
+const sendLocation = document.querySelector('#send-location');
 
 sendButton.addEventListener('click', () => {
-  socket.emit('sentMessage', messageContent.value);
+  socket.emit('sendMessage', messageContent.value, error => {
+    if (error) {
+      return console.log('error');
+    }
+    console.log(`message was delivered`);
+  });
 });
 
-const sendLocation = document.querySelector('#send-location');
 sendLocation.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation isnt supported');
   }
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      error => {
+        if (error) {
+          return console.log('An error occured');
+        }
+        console.log(`Location has been sent`);
+      }
+    );
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,11 +13,25 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   socket.emit('message', 'Welcome!');
-  socket.on('sentMessage', (msg) => {
+
+  socket.on('sendMessage', (msg, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(msg)) {
+      return callback('Profanity isnt allowed!');
+    }
     socket.broadcast.emit('message', msg);
+    callback();
   });
+
+  socket.on('sendLocation', (position, callback) => {
+    socket.broadcast.emit(
+      'message',
+      `A new user joined from  ${(position.latitude, position.longitude)}`
+    );
+  });
+
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left');
   });
